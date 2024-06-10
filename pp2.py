afb618f58365c46a53ec9a3a35fbdad16091867e
aisara28 <123066272+aisara28@users.noreply.github.com>
2024-06-10 10:12:27 +0500

    Add files via upload

--- pp2.py
+++ pp2.py
@@ -1,145 +1,266 @@
-#PYTHON SYNTAX
+#PYTHON Booleans
 
+"""The statement below would print a Boolean value, which one?"""
+print(10 > 9)
+#True
 
-"""Insert the missing part of the code below to output "Hello World"."""
-print("Hello, World")
+"""The statement below would print a Boolean value, which one?"""
+print(10 == 9)
+#False
+
+"""The statement below would print a Boolean value, which one?"""
+print(10 < 9)
+#False
+
+"""The statement below would print a Boolean value, which one?"""
+print(bool("abc"))
+#True
+
+"""The statement below would print a Boolean value, which one?"""
+print(bool(0))
+#False
+
+#PYTHON Operators
+
+"""Multiply 10 with 5, and print the result."""
+print(10*5)
+
+"""Divide 10 by 2, and print the result."""
+print(10/2)
+
+"""Use the correct membership operator to check if "apple" is present in the fruits object."""
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+"""Use the correct comparison operator to check if 5 is not equal to 10."""
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+"""Use the correct logical operator to check if at least one of two statements is True."""
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+
+#PYTHON Lists
+
+"""Print the second item in the fruits list."""
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+"""Change the value from "apple" to "kiwi", in the fruits list."""
+fruits = ["apple", "banana", "cherry"]
+fruits[0]= "kiwi"
+
+"""Use the append method to add "orange" to the fruits list."""
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+"""Use the insert method to add "lemon" as the second item in the fruits list."""
+fruits = ["apple", "banana", "cherry"]
+fruits.insert("lemon")
+
+"""Use the remove method to remove "banana" from the fruits list."""
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+"""Use negative indexing to print the last item in the list."""
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+"""Use a range of indexes to print the third, fourth, and fifth item in the list."""
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+"""Use the correct syntax to print the number of items in the list."""
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
+#PYTHON Tuples
+"""Use the correct syntax to print the first item in the fruits tuple."""
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+"""Use the correct syntax to print the number of items in the fruits tuple."""
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+"""Use negative indexing to print the last item in the tuple."""
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+"""Use a range of indexes to print the third, fourth, and fifth item in the tuple."""
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+
+#PYTHON Sets
+
+"""Check if "apple" is present in the fruits set."""
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+"""Use the add method to add "orange" to the fruits set."""
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+"""Use the correct method to add multiple items (more_fruits) to the fruits set."""
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+"""Use the remove method to remove "banana" from the fruits set."""
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+"""Use the discard method to remove "banana" from the fruits set."""
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
+#PYTHON Dictionary
+
+"""Use the get method to print the value of the "model" key of the car dictionary."""
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+"""Add the key/value pair "color" : "red" to the car dictionary."""
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"]="red"
+
+"""Use the pop method to remove "model" from the car dictionary."""
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+"""Use the clear method to empty the car dictionary"""
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+
+#PYTHON If...Else
+
+"""Print "Hello World" if a is greater than b."""
+a = 50
+b = 10
+if a >b:
+  print("Hello World")
+
+"""Print "Hello World" if a is not equal to b."""
+a = 50
+b = 10
+if a !=b:
+  print("Hello World")
+
+"""Print "Yes" if a is equal to b, otherwise print "No"."""
+a = 50
+b = 10
+if a ==b:
+
+  print("Yes")
+else:
+  print("No")
+
+"""Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3"."""
+a = 50
+b = 10
+if a ==b:
+  print("1")
+elif a >b:
+  print("2")
+else:
+  print("3")
+
+"""Print "Hello" if a is equal to b, or if c is equal to d."""
+c=24
+d = 45
+if a == b or c == d:
+  print("Hello")
 
 """Complete the code block, print "YES" if 5 is larger than 2.
- Hint: remember the indentation."""
+
+Hint: remember the indentation."""
 if 5 > 2:
     print("YES")
 
-#PYTHON COMMENTS
+"""Use the correct one line short hand syntax to print "YES" if a is equal to b, otherwise print("NO")."""
+a = 2
+b = 5
+print("YES") if a == b  else  print("NO")
 
+"""Use an if statement to print "YES" if either a or b is equal to c."""
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
 
-"""Comments in Python are written with a special character, which one?"""
-#This is a comment
+  print("YES")
 
-#Use a multiline string to make the a multiline comment:#
-"""This is a comment
-written in 
-more than just one line"""
+#PYTHON While Loops 
 
+"""Print i as long as i is less than 6."""
+i = 1
+while i < 6:
+  print(i)
+  i += 1
 
-#PYTHON VARIABLES
+"""Stop the loop if i is 3.""" 
+i = 1
+while i < 6:
+  if i == 3:
+    break
+  i += 1
 
-'''Create a variable named carname and assign the value Volvo to it.'''
-carname = "Volvo"
+"""In the loop, when i is 3, jump directly to the next iteration.""" 
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i) 
 
-'''Create a variable named x and assign the value 50 to it.'''
-x=50
+"""Print a message once the condition is false."""
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
 
-'''Display the sum of 5 + 10, using two variables: x and y.'''
-x=5
-y=10
-print(x+y)
+  print("i is no longer less than 6")  
 
-'''Create a variable called z, assign x + y to it, and display the result.'''
-x = 5
-y = 10
-z = x + y
-print(z)
+#PYTHON For Loop
 
-'''Insert the correct syntax to assign values to multiple variables in one line:'''
-x,y,z= "Orange", "Banana", "Cherry"
+"""Loop through the items in the fruits list."""
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
 
-'''Insert the correct syntax to assign the same value to all three variables in one code line.'''
-x = y = z = "Orange"
+"""In the loop, when the item value is "banana", jump directly to the next item."""
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
 
-'''Insert the correct keyword to make the variable x belong to the global scope.'''
-def myfunc():
-  global x
-  x = "fantastic"
+"""Use the range function to loop through a code set 6 times"""
+for x in range(6):
+  print(x)
 
-
-
-#PYTHON DATA TYPES
-
-'''The following code example would print the data type of x, what data type would that be?'''
-x = 5
-print(type(x))
-#int
-
-'''The following code example would print the data type of x, what data type would that be?'''
-x = "Hello World"
-print(type(x))
-#str
-
-'''The following code example would print the data type of x, what data type would that be?'''
-x = 20.5
-print(type(x))
-#float
-
-'''The following code example would print the data type of x, what data type would that be?'''
-x = ["apple", "banana", "cherry"]
-print(type(x))
-#list
-
-'''The following code example would print the data type of x, what data type would that be?'''
-x = ("apple", "banana", "cherry")
-print(type(x))
-#tuple
-
-'''The following code example would print the data type of x, what data type would that be?'''
-x = {"name" : "John", "age" : 36}
-print(type(x))
-#dict
-
-'''The following code example would print the data type of x, what data type would that be?'''
-x = True
-print(type(x))
-#bool
-
-
-
-#PYTHON NUMBERS
-
-'''Insert the correct syntax to convert x into a floating point number.'''
-x = 5
-x = float(x)
-
-'''Insert the correct syntax to convert x into a integer.'''
-x = 5.5
-x = int(x)
-
-'''Insert the correct syntax to convert x into a complex number.'''
-x = 5
-x = complex(x)
-
-#PYTHON STRINGS
-
-'''Use the len function to print the length of the string.'''
-x = "Hello World"
-print(len(x))
-
-'''Get the first character of the string txt.'''
-txt = "Hello World"
-x = txt[0]
-
-'''Get the characters from index 2 to index 4 (llo).'''
-txt = "Hello World"
-x = txt[2:5]
-
-'''Return the string without any whitespace at the beginning or the end.'''
-txt = " Hello World "
-x = txt.strip()
-
-'''Convert the value of txt to upper case.'''
-txt = "Hello World"
-txt = txt.upper()
-
-'''Convert the value of txt to lower case.'''
-txt = "Hello World"
-txt = txt.lower()
-
-'''Replace the character H with a J.'''
-txt = "Hello World"
-txt = txt.replace('H','J')
-
-'''Replace the character H with a J.'''
-age = 36
-txt = "My name is John, and I am {}"
-print(txt.format(age))
-
-
-
+"""Exit the loop when x is "banana"."""
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
